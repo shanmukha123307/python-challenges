9003c1c305e7b87e731602b4242157bab4410fa8
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-02 11:57:16 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,6 @@
+n=int(input("enter the numrator"))
+d=int(input("enter the dinominator"))
+if n%d==0:
+    print("\n" +str(n)+"is divisibl by" +str(d))
+else:
+    print("\n" +str(n)+"is not divisibl by" +str(d))
