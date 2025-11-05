e6c06fedf7e4d0d20f86ca0f0b6e42e5252ce096
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-05 10:02:07 +0530

    Delete file3.py

--- file3.py
+++
@@ -1,9 +0,0 @@
-meen1=38
-wrong_number=36
-correct_number=56
-total_number=40
-sum=meen1*total_number
-print("the sum of 40 number is ",sum)
-num2=sum-((wrong_number)-(correct_number))
-meen2=num2/total_number
-print(meen2)
