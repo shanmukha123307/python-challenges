beaafc2a654a9d374b3e52c42c85ba35f21a929b
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-05 10:01:57 +0530

    Delete file2.py

--- file2.py
+++
@@ -1,6 +0,0 @@
-n=int(input("enter the numrator"))
-d=int(input("enter the dinominator"))
-if n%d==0:
-    print("\n" +str(n)+"is divisibl by" +str(d))
-else:
-    print("\n" +str(n)+"is not divisibl by" +str(d))
